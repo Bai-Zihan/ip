73d7bf6139fc7f9158da1b7b67e8be7cd8adaf12
Bai Zihan <e1459259@u.nus.edu>
2026-01-18 17:32:19 +0800

    Level 2. Add, List

--- src/main/java/Lime.java
+++ src/main/java/Lime.java
@@ -5,6 +5,8 @@ public class Lime {
         String name = "Lime";
         String horizontalLine = "   ____________________________________________________________";
         Scanner sc = new Scanner(System.in);
+        String[] toDo = new String[100];
+        int count = 1;
 
         System.out.println(horizontalLine);
         System.out.println("    Hello! I'm " + name);
@@ -14,7 +16,19 @@ public class Lime {
         String command = sc.nextLine();
         while (!command.equals("bye")) {
             System.out.println(horizontalLine);
-            System.out.println("    " + command);
+
+            if (command.equals("list")) {
+                for (int i = 0; i < toDo.length; i++) {
+                    if (toDo[i] == null) {
+                        break;
+                    }
+                    System.out.println("     " + (i + 1) + ". " + toDo[i]);                }
+            } else {
+                System.out.println("    " + "added: " + command);
+                toDo[count - 1] = command;
+                count++;
+            }
+
             System.out.println(horizontalLine);
             command = sc.nextLine();
         }
