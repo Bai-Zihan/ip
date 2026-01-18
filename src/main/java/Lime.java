85c3f31f5ee4d7aaf992e8be3a3fa10662d5421a
Bai Zihan <e1459259@u.nus.edu>
2026-01-18 17:04:02 +0800

    Level 1. Echo

--- src/main/java/Lime.java
+++ src/main/java/Lime.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class Lime {
     public static void main(String[] args) {
         String name = "Lime";
-        String horizontalLine = "____________________________________________________________";
+        String horizontalLine = "   ____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(horizontalLine);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm " + name);
+        System.out.println("    What can I do for you?");
         System.out.println(horizontalLine);
 
-        System.out.println("Bye. Hope to see you again soon!");
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(horizontalLine);
+            System.out.println("    " + command);
+            System.out.println(horizontalLine);
+            command = sc.nextLine();
+        }
+
+        System.out.println(horizontalLine);
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
     }
 }
